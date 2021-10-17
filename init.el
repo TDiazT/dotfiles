013f6d9d3876cfcf6c88e689bd0c40c6dc83b4ce
Tomas <tomdtron@gmail.com>
2021-10-17 12:55:52 -0300

    func: Add group keybinds to toggle theme

--- init.el
+++ init.el
@@ -314,6 +314,11 @@
 (tdtron/leader-keys
   "s" '(hydra-text-scale/body :which-key "scale text"))
 
+(tdtron/leader-keys
+ "t"  '(:ignore t :which-key "toggles")
+ "tw" 'whitespace-mode
+ "tt" '(counsel-load-theme :which-key "choose theme"))
+
 (defun tdtron/org-font-setup ()
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
@@ -477,6 +482,20 @@
          ("C-M-i" . completion-at-point)
          )
   )
+
+;; (use-package org-roam-ui
+;;   ;; :straight
+;;   ;;   (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+;;     :after org-roam
+;; ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;; ;;         a hookable mode anymore, you're advised to pick something yourself
+;; ;;         if you don't care about startup time, use
+;; ;;  :hook (after-init . org-roam-ui-mode)
+;;     :config
+;;     (setq org-roam-ui-sync-theme t
+;;           org-roam-ui-follow t
+;;           org-roam-ui-update-on-save t
+;;           org-roam-ui-open-on-start t))
 
 ;; Ivy
 (use-package ivy
