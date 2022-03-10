427f2741f108e7cfc079b774664f986df40cdc55
Tomas <tomdtron@gmail.com>
2022-03-10 15:31:49 -0300

    feat: Add LSP support + racket

--- init.el
+++ init.el
@@ -613,6 +613,44 @@
 (setq history-length 25)
 (savehist-mode 1)
 
+(save-place-mode 1)
+
+(defun tdtron/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . tdtron/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
 (defun tdtron/switch-project-action ()
   "Switch to a workspace with the project name and start `magit-status'."
   ;; TODO: Switch to EXWM workspace 1?
@@ -695,6 +733,9 @@
 (use-package tuareg)
 
 (use-package racket-mode
-  :hook (racket-mode . racket-xp-mode)
   :mode "\\.rkt\\'"
-  )
+  :hook (racket-mode . racket-xp-mode)   )
+
+(with-eval-after-load 'lsp-mode
+  (require 'lsp-racket)
+  (add-hook 'racket-mode-hook #'lsp))
