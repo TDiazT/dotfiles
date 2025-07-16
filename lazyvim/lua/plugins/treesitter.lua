34a0ccb6e6b969a6b79a75ff2b8418138deeda3c
Tomas <tomdtron@gmail.com>
2025-07-16 20:33:07 +0200

    lazyvim: Update treesitter base parsers

--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -5,6 +5,10 @@ return {
     vim.list_extend(opts.ensure_installed, {
       "ocaml",
       "ocaml_interface",
+      "latex",
+      "bibtex",
+      "gitcommit",
+      "gitignore",
     })
   end,
 }
