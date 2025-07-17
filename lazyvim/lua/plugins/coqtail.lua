06c82ef321538a25dbbae2de8bba5d326e1f3593
Tomas <tomdtron@gmail.com>
2025-07-17 11:38:43 +0200

    lazyvim: Add keymaps for coqtail

--- lazyvim/lua/plugins/coqtail.lua
+++ lazyvim/lua/plugins/coqtail.lua
@@ -1,3 +1,22 @@
 return {
   "whonore/Coqtail",
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.add({
+      mode = { "n" },
+      { "<leader>r", group = "Rocq" },
+      { "<leader>rj", "<cmd>RocqNext<cr>", desc = "Step forward" },
+      { "<leader>rk", "<cmd>RocqUndo<cr>", desc = "Step back" },
+      { "<leader>r<cr>", "<cmd>RocqToLine<cr>", desc = "Step to point" },
+      { "<leader>rs", "<cmd>RocqStop<cr>", desc = "Stop" },
+      { "<leader>ri", "<cmd>RocqInterrupt<cr>", desc = "Interrupt" },
+      {
+        "<leader>r<space>",
+        function()
+          wk.show({ keys = "<leader>r", loop = true })
+        end,
+        desc = "Rocq Hydra Mode (which-key)",
+      },
+    }, opts)
+  end,
 }
