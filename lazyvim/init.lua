137d00110baddd707b529ca53e512016011b9e55
Tomas <tomdtron@gmail.com>
2025-07-21 11:02:16 +0200

    lazyvim: First setup Godot

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.godot")
 
 local lspconfig = require("lspconfig")
 lspconfig.ocamllsp.setup({})
+lspconfig.gdscript.setup({})
