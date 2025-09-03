d9141c664c3a8ee925408e7eabfd79c72d7c4219
Tomas <tomdtron@gmail.com>
2025-09-03 16:32:07 +0200

    lazy: Change Coqtail bindings

--- lazyvim/lua/plugins/coqtail.lua
+++ lazyvim/lua/plugins/coqtail.lua
@@ -3,17 +3,17 @@ return {
   config = function(_, opts)
     local wk = require("which-key")
     wk.add({
-      mode = { "n" },
-      { "<leader>r", group = "Rocq" },
-      { "<leader>rj", "<cmd>RocqNext<cr>", desc = "Step forward" },
-      { "<leader>rk", "<cmd>RocqUndo<cr>", desc = "Step back" },
-      { "<leader>r<cr>", "<cmd>RocqToLine<cr>", desc = "Step to point" },
-      { "<leader>rs", "<cmd>RocqStop<cr>", desc = "Stop" },
-      { "<leader>ri", "<cmd>RocqInterrupt<cr>", desc = "Interrupt" },
+      mode = { "n", "i" },
+      { "<C-r>", group = "Rocq" },
+      { "<C-r><C-j>", "<cmd>RocqNext<cr>", desc = "Step forward" },
+      { "<C-r><C-k>", "<cmd>RocqUndo<cr>", desc = "Step back" },
+      { "<C-r><C-cr>", "<cmd>RocqToLine<cr>", desc = "Step to point" },
+      { "<C-r><C-s>", "<cmd>RocqStop<cr>", desc = "Stop" },
+      { "<C-r><C-i>", "<cmd>RocqInterrupt<cr>", desc = "Interrupt" },
       {
-        "<leader>r<space>",
+        "<C-r><space>",
         function()
-          wk.show({ keys = "<leader>r", loop = true })
+          wk.show({ keys = "<C-r>", loop = true })
         end,
         desc = "Rocq Hydra Mode (which-key)",
       },
