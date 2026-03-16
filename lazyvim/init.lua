6dc5785c16f30434199727e9fb094330a15075e2
Tomás Díaz <tomdtron@gmail.com>
2026-03-16 14:33:45 +0100

    lazy: Add C++ support + Cmake

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -10,3 +10,24 @@ vim.lsp.config("ocamllsp", {
 })
 
 vim.lsp.enable("ocamllsp")
+
+-- vim.lsp.config("cmake").setup({})
+vim.lsp.enable("cmake")
+
+local clangd = vim.fn.has("mac") == 1 and "/opt/homebrew/opt/llvm/bin/clangd" or "clangd"
+
+vim.lsp.config("clangd", {
+  cmd = {
+    clangd,
+    "--compile-commands-dir=build",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders=1",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_markers = { "compile_commands.json", "CMakeLists.txt", ".git" },
+})
+
+vim.lsp.enable("clangd")
--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -1,5 +1,19 @@
+-- Possible solution to problems when updating ?
+-- 1. Treesitter parser .so files in ~/.local/share/nvim/site/parser/ with invalid code signatures (macOS kills nvim when loading them)
+-- 2. blink.cmp's pre-built binary also needed to be downloaded fresh
+local function codesign_parsers()
+  if vim.fn.has("mac") == 1 then
+    local parser_dir = vim.fn.stdpath("data") .. "/site/parser"
+    vim.fn.system("codesign -f -s - " .. parser_dir .. "/*.so 2>/dev/null")
+  end
+end
+
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = function()
+    vim.cmd("TSUpdate")
+    codesign_parsers()
+  end,
   opts = function(_, opts)
     -- add Ocaml
     vim.list_extend(opts.ensure_installed, {
@@ -8,6 +22,9 @@ return {
       "bibtex",
       "gitcommit",
       "gitignore",
+      "cpp",
+      "c",
+      "cmake",
     })
   end,
 }
